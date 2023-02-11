dfc4e1087cd1d14d490fbeef55a991c9e29b4045
Sofiia Kolomiichuk <gungster333@gmail.com>
2023-02-11 13:41:38 +0100

    second commit

---
+++ hw-2/index.js
@@ -0,0 +1,8 @@
+let number = prompt('Введите число');
+let number2 = prompt('Введите число');
+
+alert (+number + +number2);
+alert (+number - +number2);
+alert (+number * +number2);
+alert (+number / +number2);
+

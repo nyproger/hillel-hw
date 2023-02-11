e9bd16176e06917e6e993a24cc2dfeecba9568d5
Sofiia Kolomiichuk <gungster333@gmail.com>
2023-02-11 13:56:38 +0100

    second commit

---
+++ hw-2/hw2.2/index.js
@@ -0,0 +1,7 @@
+let hours = prompt('Введите количество часов');
+
+console.log(+hours *3600);
+
+let minutes = (+hours *3600)
+
+alert (minutes);
---
+++ hw-2/hw2.3/index.js
@@ -0,0 +1,5 @@
+let a = prompt('Введите число');
+let b = prompt('Введите число');
+let c = prompt('Введите число');
+
+alert ((+a + +b + +c)/3)
